7dfc2e09bf5cf9fe2d9c8e29b0a9ee5f731466a0
Habib Khan <mkhk352@gmail.com>
2017-12-25 19:01:07 -0600

    menu item showing

---
+++ ToDoTimer/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <AppHeader/>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+export default {
+  name: 'app',
+  components: {
+    AppHeader: AppHeader
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ ToDoTimer/src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <sui-menu :widths="2" :activeIndex="clicked" :pointing="true">
+    <sui-menu-item>Home</sui-menu-item>
+    <sui-menu-item>About</sui-menu-item>
+  </sui-menu>
+</template>
+
+<script>
+  export default{
+    name: 'AppHeader',
+
+    data () {
+      return {
+
+      }
+    },
+    methods () {
+      return {
+        clicked: function(){
+
+        }
+      }
+    }
+  }
+</script>
